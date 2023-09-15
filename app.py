b3677cc6f37cabf2182c05f857daadf5f44e8ffc
mainangaruiya <003paulmaina@gmail.com>
2023-09-15 16:26:48 +0300

    fix mvp

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 import time
 import sched
+from datetime import datetime
 
 app = Flask(__name__)
 s = sched.scheduler(time.time, time.sleep)
@@ -8,6 +9,8 @@ reminders = []
 
 @app.route('/')
 def index():
+    check_overdue_tasks()#overdue task checker
+    update_time_remaining()#for each tash
     return render_template('index.html', reminders=reminders)
 
 @app.route('/add', methods=['POST'])
@@ -26,6 +29,25 @@ def add():
 
 def remind(task):
     print(f"Reminder: '{task}' is due in 2 hours!")
+
+def check_overdue_tasks():
+    now = datetime.now()
+    for reminder in reminders:
+        due_time = datetime.strptime(reminder['time'], "%Y-%m-%dT%H:%M")
+        if now > due_time:
+            reminder['overdue'] = True
+        else:
+            reminder['overdue'] = False
+
+def update_time_remaining():
+    now = datetime.now()
+    for reminder in reminders:
+        due_time = datetime.strptime(reminder['time'], "%Y-%m-%dT%H:%M")
+        if now < due_time:
+            time_diff = due_time - now
+            reminder['time_remaining'] = time_diff.total_seconds()
+        else:
+            reminder['time_remaining'] = 0
 
 if __name__ == '__main__':
     app.run(debug=True)
